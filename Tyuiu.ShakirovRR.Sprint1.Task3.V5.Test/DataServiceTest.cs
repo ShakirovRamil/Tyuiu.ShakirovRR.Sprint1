7b57d9df0af6a8eef7efc7dbb6f7c8a539675983
Ramil Shakirov <ram.shok@bk.ru>
2023-09-23 00:05:08 +0500

    Task3

---
+++ Tyuiu.ShakirovRR.Sprint1.Task3.V5.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.ShakirovRR.Sprint1.Task3.V5.Lib;
+
+namespace Tyuiu.ShakirovRR.Sprint1.Task3.V5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double mapScale = 2;
+            double distanceBetweenPoints = 3;
+            double distanceBetweenCitys = 6;
+            var res = ds.DistanceLength(mapScale, distanceBetweenPoints, distanceBetweenCitys);
+            Assert.AreEqual(distanceBetweenCitys, res);
+        }
+    }
+}
---
+++ Tyuiu.ShakirovRR.Sprint1.Task3.V5/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ShakirovRR.Sprint1.Task3.V5.Lib;
+
+namespace Tyuiu.ShakirovRR.Sprint1.Task3.V5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Шакиров Р. Р. | ИСТНБ-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навки работы в C#                                         *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Выполнил: Шакиров Рамиль Русланович | ИСТНБ-23-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет расстояние между городами.                                    *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double a = 120;
+            double b = 3.5;
+            double c = 420;
+
+            Console.WriteLine("Масштабирование карты = " + a);
+            Console.WriteLine("расстояние между точками на карте = " + b);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("расстояние между городами = " + ds.DistanceLength(a, b, c));
+
+            Console.ReadLine();
+        }
+    }
+}
